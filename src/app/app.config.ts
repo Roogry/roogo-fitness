ba0ff9f525ce272d77028ee29b17af8c1b0a4d2f
Roogry <jodiemantra@gmail.com>
2026-03-09 00:38:26 +0700

    feat: Add PWA support with Angular Service Worker, web app manifest, and launcher icons.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideZard } from '@/shared/core/provider/providezard';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideZard(),]
+    provideZard(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
 };
